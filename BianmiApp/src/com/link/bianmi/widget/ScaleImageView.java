be955b31197e2b63d84555a1afae9537b299fb25
pangfq <littlerhub@yeah.net>
2014-07-15 10:52:12 +0800

    .

--- BianmiApp/src/com/link/bianmi/widget/ScaleImageView.java
+++ BianmiApp/src/com/link/bianmi/widget/ScaleImageView.java
@@ -40,19 +40,16 @@ public class ScaleImageView extends TopCropImageView{
 
 	public ScaleImageView(Context context) {
 		super(context);
-		// TODO Auto-generated constructor stub
 //		init();
 	}
 
 	public ScaleImageView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		// TODO Auto-generated constructor stub
 //		init();
 	}
 
 	public ScaleImageView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		// TODO Auto-generated constructor stub
 //		init();
 	}
 	
