2c9bcc32daa419a102649ae4d48038968a20d0cd
pangfq <littlerhub@yeah.net>
2014-08-15 14:35:46 +0800

    A:LockScreenActivity,but has some bugs.

--- BianmiApp/src/com/link/bianmi/activity/SettingsActivity.java
+++ BianmiApp/src/com/link/bianmi/activity/SettingsActivity.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 import com.link.bianmi.R;
 import com.link.bianmi.UserConfig;
 import com.link.bianmi.activity.base.BaseFragmentActivity;
+import com.link.bianmi.widget.SwitchButton;
 
 public class SettingsActivity extends BaseFragmentActivity {
 
@@ -19,10 +22,19 @@ public class SettingsActivity extends BaseFragmentActivity {
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		setContentView(R.layout.activity_settings);
 
+		// 设置密码
+		SwitchButton passSwitchBtn = (SwitchButton) findViewById(R.id.settings_item_password_switchbutton);
+		passSwitchBtn.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				launchActivity(LockScreenActivity.class);
+			}
+		});
+
 		// 退出登录
 		findViewById(R.id.settings_item_exit_group).setOnClickListener(
 				new OnClickListener() {
-
 					@Override
 					public void onClick(View v) {
 						UserConfig.getInstance().setSessionId("");
